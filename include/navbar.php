ecfc294522c22a64e2940fb206ef659090d4d23b
Grigorio Guterres Gusmao <86569634+GrigorioGuterres@users.noreply.github.com>
2024-02-26 11:17:44 -0800

    DADUS KOMPLETU

---
+++ include/navbar.php
@@ -0,0 +1,13 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light shadow" style="background: rgb(19,184,194);
+background: -moz-linear-gradient(90deg, rgba(19,184,194,1) 6%, rgba(194,19,145,1) 60%, rgba(151,19,194,1) 87%);
+background: -webkit-linear-gradient(90deg, rgba(19,184,194,1) 6%, rgba(194,19,145,1) 60%, rgba(151,19,194,1) 87%);
+background: linear-gradient(90deg, rgba(19,184,194,1) 6%, rgba(194,19,145,1) 60%, rgba(151,19,194,1) 87%);
+filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#13b8c2",endColorstr="#9713c2",GradientType=1);">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+          <i class="fas fa-th-large"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
